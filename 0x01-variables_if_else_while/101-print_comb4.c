f9491ca7455b9187cb1967669fdfd4d3b62ddc15
eddykeys <eddyonkeys@gmail.com>
2022-07-03 23:42:11 -0700

    task 10 redone

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - Entyr point
- * prints two digits combination
- * Return: Always 0 (success)
+ * main - Prints 3 combination of numbers
+ * Return: Always (Success)
  */
 int main(void)
 {
-int c, i;
+int c, i, k;
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
 {
-if (c < i)
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)
 {
 putchar(c);
 putchar(i);
-if (c != '8' || (c == '8' && i != '9'))
+putchar(k);
+if (c != '7')
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
