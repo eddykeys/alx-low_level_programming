2620a86f7b4b5149fd3f00857c531fb8381dbd12
eddykeys <eddyonkeys@gmail.com>
2022-07-21 15:00:14 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,17 +2,17 @@
 /**
  * print_chessboard - function that prints the chessboard
  * @a: Pointer to 2-D array location
- *  Return: Null
- *   */
+ * Return: Null
+ */
 void print_chessboard(char (*a)[8])
 {
 	int i;
-		int j;
+	int j;
 
-		for (i = 0; i < 8; i++)
-		{
-			for (j = 0; j < 8; j++)
-				putchar(a[i][j]);
-			putchar('\n');
-		}
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			putchar(a[i][j]);
+		putchar('\n');
+	}
 }
